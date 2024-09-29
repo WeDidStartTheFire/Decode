8edde5932861267d8354be5b96626b537f778752
Owen <27orinne@granvilleschools.org>
2024-09-29 16:56:19 -0400

    Odometry calibrations

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -46,7 +46,7 @@ public class DriveConstants {
      * convenience. Make sure to exclude any gear ratio included in MOTOR_CONFIG from GEAR_RATIO.
      */
     public static double WHEEL_RADIUS = 1.88976; // in, for larger wheels
-    public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
+    public static double GEAR_RATIO = 1.0041505558356485; // output (wheel) speed / input (motor) speed
     public static double TRACK_WIDTH = 15.968504; // in, not filled out
 
     /*

8f2a1e0780c26501d6e917e146278c7f31e6a6fa
Owen <27orinne@granvilleschools.org>
2024-10-06 15:00:27 -0400

    Initial Autos

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_NetZone.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "Net Zone", group = "IntoTheDeep", preselectTeleOp = "Main")
+public class Auto_NetZone extends Base {
+    @Override
+    public void runOpMode() {
+        setup(false);
+        strafe(18, dir.left);
+    }
+}

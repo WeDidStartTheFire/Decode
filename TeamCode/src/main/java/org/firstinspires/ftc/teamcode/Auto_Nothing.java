c3860dbad3d3df6dabe6370770d7816d70db7a9b
WeDidStartTheFire <ftcwedidthestartthefire@gmail.com>
2024-10-19 09:53:27 -0400

    Nothing Autonomous Added

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Nothing.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "Nothing", group = "IntoTheDeep", preselectTeleOp = "Main")
+public class Auto_Nothing extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        waitForStart();
+        while (opModeIsActive());
+    }
+}

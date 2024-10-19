5ac9b13b473ee1af6eabd8473af0b0e7108a682e
Owen <27orinne@granvilleschools.org>
2024-10-19 09:15:54 -0400

    Auto_NetZone updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_NetZone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_NetZone.java
@@ -8,14 +8,15 @@ public class Auto_NetZone extends Base {
     @Override
     public void runOpMode() {
         setup();
+        strafe(3, right);
         drive(48, forward);
         strafe(12, left);
-        drive(45, backward);
+        drive(47, backward);
         drive(45, forward);
-        strafe(12, left);
+        strafe(13, left);
         drive(40, backward);
         drive(40, forward);
-        strafe(10, left);
+        strafe(11, left);
         drive(36, backward);
         strafe(24, right);
         drive(10, backward);

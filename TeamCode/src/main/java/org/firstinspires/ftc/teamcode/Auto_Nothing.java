9732c2f72f0119fb3ee46a379ccd6237a221f345
Owen-FTC <27orinne@gmail.com>
2024-12-14 19:26:57 -0500

    Disabled and deprecated the nothing Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Nothing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Nothing.java
@@ -1,8 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+// If you want the robot to sit somewhere, you can just decide to not run a TeleOp. We can enable
+// this if we need a motor or servo to be held during Auto.
+@Disabled
+@Deprecated
 @Autonomous(name = "Nothing", group = "IntoTheDeep", preselectTeleOp = "Main")
 public class Auto_Nothing extends LinearOpMode {
     @Override

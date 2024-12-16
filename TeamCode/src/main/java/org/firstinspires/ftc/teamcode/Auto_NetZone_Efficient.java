8aa904aaf8b8769e6de126bf39b9b7e3d285de0e
Owen-FTC <27orinne@gmail.com>
2024-12-15 20:59:13 -0500

    Working on continuous motion and therefore more efficient auto, WIP

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_NetZone_Efficient.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import static org.firstinspires.ftc.teamcode.Base.Dir.*;
+
+@Autonomous(name = "Net Zone Efficient", group = "IntoTheDeep", preselectTeleOp = "Main")
+public class Auto_NetZone_Efficient extends Base {
+    Trajectory traj;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        setup();
+        strafe(6, LEFT);
+        traj = mecDrive.trajectoryBuilder(currentPose)
+                .splineTo(new Vector2d(2, 52), 0)
+                .splineTo(new Vector2d(-6, 52), 0)
+                .splineTo(new Vector2d(-6, 1), 0)
+                .build();
+        mecDrive.followTrajectory(traj);
+        currentPose = traj.end();
+//        strafe(8, RIGHT);
+//        drive(52, FORWARD);
+//        strafe(8, LEFT);
+//        drive(51, BACKWARD);
+        drive(49, FORWARD);
+        strafe(12, LEFT);
+        drive(47, BACKWARD);
+        drive(50, FORWARD);
+        strafe(7, LEFT);
+        drive(41, BACKWARD);
+        strafe(103, RIGHT);
+        drive(8, BACKWARD);
+    }
+}

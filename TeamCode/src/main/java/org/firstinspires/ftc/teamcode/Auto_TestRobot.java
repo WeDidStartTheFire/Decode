d2534d13a1d53a53813e29691fa3b6bf9287e5e9
Owen-FTC <27orinne@gmail.com>
2025-01-01 19:05:42 -0500

    Condensed code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_TestRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_TestRobot.java
@@ -21,93 +21,63 @@ public class Auto_TestRobot extends Base {
                 IMUTurn(90, LEFT);
                 confirm("Please confirm the robot turned 90° right and then 90° left back");
             }
-        } else {
-            print("Skipped turn test");
-        }
+        } else print("Turn test skipped");
 
-        if (lf != null && useOdometry) {
-            if (confirm("Test")) {
-                traj = drive.trajectoryBuilder(new Pose2d()).splineTo(new Vector2d(30, 30), 0).build();
+        if (lf != null && useOdometry && confirm("Test")) {
+            traj = drive.trajectoryBuilder(new Pose2d()).splineTo(new Vector2d(30, 30), 0).build();
 
-                drive.followTrajectory(traj);
+            drive.followTrajectory(traj);
 
-                traj = drive.trajectoryBuilder(traj.end(), true)
-                        .splineTo(new Vector2d(0, 0), Math.toRadians(180))
-                        .build();
+            traj = drive.trajectoryBuilder(traj.end(), true)
+                    .splineTo(new Vector2d(0, 0), Math.toRadians(180))
+                    .build();
 
-                drive.followTrajectory(traj);
-                confirm("Please confirm that the robot correctly followed a spline and back.");
-            }
-        } else {
-            print("Spline test skipped");
-        }
+            drive.followTrajectory(traj);
+            confirm("Please confirm that the robot correctly followed a spline and back.");
+        } else print("Spline test skipped");
 
-        if (wristMotor != null) {
-            if (confirm("Test wrist motor?")) {
-                extendWrist();
-                s(.5);
-                retractWrist();
-                s(.5);
-                confirm("Please confirm the wrist motor extended and retracted");
-            }
-        } else {
-            print("Wrist motor test skipped");
-        }
+        if (wristMotor != null && confirm("Test wrist motor?")) {
+            extendWrist();
+            s(.5);
+            retractWrist();
+            s(.5);
+            confirm("Please confirm the wrist motor extended and retracted");
+        } else print("Wrist motor test skipped");
 
-        if (wristServo != null) {
-            if (confirm("Test wrist servo?")) {
-                moveWristServo(0);
-                s(.5);
-                moveWristServo(1);
-                s(.5);
-                confirm("Please confirm the wrist servo moved from its left to right extremes");
-            }
-        } else {
-            print("Wrist servo test skipped");
-        }
+        if (wristServo != null && confirm("Test wrist servo?")) {
+            moveWristServo(0);
+            s(.5);
+            moveWristServo(1);
+            s(.5);
+            confirm("Please confirm the wrist servo moved from its left to right extremes");
+        } else print("Wrist servo test skipped");
 
-        if (intakeServo != null) {
-            if (confirm("Test intake servo?")) {
-                openIntake();
-                s(.5);
-                closeIntake();
-                s(.5);
-                confirm("Please confirm the intake servo opened and closed");
-            }
-        } else {
-            print("Intake servo test skipped");
-        }
+        if (intakeServo != null && confirm("Test intake servo?")) {
+            openIntake();
+            s(.5);
+            closeIntake();
+            s(.5);
+            confirm("Please confirm the intake servo opened and closed");
+        } else print("Intake servo test skipped");
 
-        if (liftMotor != null) {
-            if (confirm("Test horizontal lift?")) {
-                extendHorizontalLift();
-                retractHorizontalLift();
-                confirm("Please confirm the lift extended and retracted");
-            }
-        } else {
-            print("Horiontal motor test skipped");
-        }
+        if (liftMotor != null && confirm("Test horizontal lift?")) {
+            extendHorizontalLift();
+            retractHorizontalLift();
+            confirm("Please confirm the lift extended and retracted");
+        } else print("Horiontal motor test skipped");
 
-        if (verticalMotorA != null) {
-            if (confirm("Vertical lift test?")) {
-                extendVerticalLift();
-                retractVerticalLift();
-                confirm("Please confirm that the vertical lift extendend and retracted");
-            }
-        } else {
-            print("Skipped vertical lift test");
-        }
+        if (verticalMotorA != null && confirm("Vertical lift test?")) {
+            extendVerticalLift();
+            retractVerticalLift();
+            confirm("Please confirm that the vertical lift extendend and retracted");
+        } else print("Vertical lift test skipped");
 
-        if (touchSensor != null) {
-            if (confirm("Test touch sensor?")) {
-                print("Please press the touch sensor or hit B to skip");
-                update();
-                while (opModeIsActive() && !touchSensor.isPressed() && !gamepad1.b) {}
-                print("Touch sensor pressed", touchSensor.isPressed());
-            }
-        } else {
-            print("Skipped touch sensor test");
-        }
+        if (touchSensor != null && confirm("Test touch sensor?")) {
+            print("Please press the touch sensor or hit B to skip");
+            update();
+            while (opModeIsActive() && !touchSensor.isPressed() && !gamepad1.b) {}
+            print("Touch sensor pressed", touchSensor.isPressed());
+        } else print("Touch sensor test skipped");
 
         update();
     }

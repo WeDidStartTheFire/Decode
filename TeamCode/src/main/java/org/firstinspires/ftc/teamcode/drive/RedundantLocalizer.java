fab3b81e3803875976af9ab73c8b0ff20e3366b6
Owen-FTC <27orinne@gmail.com>
2025-01-04 17:29:10 -0500

    Improved RedundantLocalizer, same functionality but less lines and unneeded actions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/RedundantLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/RedundantLocalizer.java
@@ -1,68 +1,55 @@
 package org.firstinspires.ftc.teamcode.drive;
 
-import static org.firstinspires.ftc.teamcode.Base.IMU_PARAMS;
-
 import androidx.annotation.NonNull;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.localization.Localizer;
-import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class RedundantLocalizer implements Localizer {
 
-    private final Localizer sparkFunLocalizer;
-    private final Localizer encoderLocalizer;
-    private final IMU imu;
-    private boolean useSparkFun = true;
+    private final Localizer primaryLocalizer;
+    private final Localizer secondaryLocalizer;
+    private boolean usePrimary = true;
 
     public RedundantLocalizer(@NonNull HardwareMap hardwareMap) {
-        this.sparkFunLocalizer = new SparkFunLocalizer(hardwareMap);
-        List<Integer> lastTrackingEncPositions = new ArrayList<>();
-        List<Integer> lastTrackingEncVels = new ArrayList<>();
-        this.encoderLocalizer = new StandardTrackingWheelLocalizer(hardwareMap, lastTrackingEncPositions, lastTrackingEncVels);
-        imu = hardwareMap.get(IMU.class, "imu");
-        if (!imu.initialize(IMU_PARAMS)) {
-            throw new RuntimeException("IMU initialization failed");
-        }
+        this.primaryLocalizer = new SparkFunLocalizer(hardwareMap);
+        this.secondaryLocalizer = new StandardTrackingWheelLocalizer(hardwareMap, new ArrayList<>(), new ArrayList<>());
     }
 
     @Override
     public void update() {
-        sparkFunLocalizer.update();
-        encoderLocalizer.update();
+        primaryLocalizer.update();
+        secondaryLocalizer.update();
 
         // Switch if SparkFun is unreliable; Skip check if already found unreliable
-        if (!useSparkFun && !isSparkFunReliable()) {
-            useSparkFun = false;
-        }
+        if (!usePrimary && !isSparkFunReliable()) usePrimary = false;
     }
 
     @NonNull
     @Override
     public Pose2d getPoseEstimate() {
-        return useSparkFun ? sparkFunLocalizer.getPoseEstimate() : encoderLocalizer.getPoseEstimate();
+        return usePrimary ? primaryLocalizer.getPoseEstimate() : secondaryLocalizer.getPoseEstimate();
     }
 
     @Override
     public void setPoseEstimate(@NonNull Pose2d pose) {
-        sparkFunLocalizer.setPoseEstimate(pose);
-        encoderLocalizer.setPoseEstimate(pose);
+        primaryLocalizer.setPoseEstimate(pose);
+        secondaryLocalizer.setPoseEstimate(pose);
     }
 
     @Override
     public Pose2d getPoseVelocity() {
-        return useSparkFun ? sparkFunLocalizer.getPoseVelocity() : encoderLocalizer.getPoseVelocity();
+        return usePrimary ? primaryLocalizer.getPoseVelocity() : secondaryLocalizer.getPoseVelocity();
     }
 
     private boolean isSparkFunReliable() {
-        Pose2d sfPose = sparkFunLocalizer.getPoseEstimate();
-        Pose2d sfVel = sparkFunLocalizer.getPoseVelocity();
-        Pose2d encPose = encoderLocalizer.getPoseEstimate();
-        Pose2d encVel = encoderLocalizer.getPoseVelocity();
+        Pose2d sfPose = primaryLocalizer.getPoseEstimate();
+        Pose2d sfVel = primaryLocalizer.getPoseVelocity();
+        Pose2d encPose = secondaryLocalizer.getPoseEstimate();
+        Pose2d encVel = secondaryLocalizer.getPoseVelocity();
         assert sfVel != null && encVel != null;
         double[] sfMeasurements = {sfPose.getX(), sfPose.getY(), sfVel.getX(), sfVel.getY()};
         double[] encMeasurements = {encPose.getX(), encPose.getY(), encVel.getX(), encVel.getY()};
@@ -71,11 +58,8 @@ public class RedundantLocalizer implements Localizer {
         double compounding_error = 0;
         for (int i = 0; i < sfMeasurements.length; i++) {
             error =  getError(sfMeasurements[i], encMeasurements[i]);
-            if (error > 50) {
-                return false;
-            } else if (error > 20) {
-                compounding_error += error;
-            }
+            if (error > 50) return false;
+            else if (error > 20) compounding_error += error;
         }
 
         return !(compounding_error > 100);

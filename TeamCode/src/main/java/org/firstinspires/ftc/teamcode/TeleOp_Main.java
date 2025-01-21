1e29f85d73d5bde69a7f4f1e3df6f64c601cbe10
Owen-FTC <27orinne@gmail.com>
2025-01-21 12:32:30 -0500

    Lift now goes up after handoff is complete

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Main.java
@@ -35,6 +35,7 @@ public class TeleOp_Main extends Base {
     boolean wasWristServoButtonPressed = false;
     boolean wasSpecimenServoButtonPressed = false;
     boolean wasBasketServoButtonPressed = false;
+    boolean handoff = false;
     int wristMotorTicksStopped = 0;
     int wristMotorPos = 0;
     int wristMotorStopPos = 0;
@@ -129,7 +130,12 @@ public class TeleOp_Main extends Base {
                     else liftIn = true;
                     slow = abs(liftPos - liftGoal) < 50;
                     liftRunToPos = abs(liftPos - liftGoal) > 20;
-                    if (!liftRunToPos && intakeServo != null) openIntake();
+                    if (!liftRunToPos && intakeServo != null && handoff) {
+                        handoff = false;
+                        openIntake();
+                        vertRunToPos = true;
+                        vertGoal = V_LIFT_GOALS[3];
+                    }
                 }
                 slow = gamepad1.left_bumper || slow;
                 liftOut = liftOut || gamepad1.dpad_right;
@@ -240,6 +246,7 @@ public class TeleOp_Main extends Base {
             wasSpecimenServoButtonPressed = gamepad1.b;
 
             if (gamepad2.y) {
+                handoff = true;
                 wristMotorTicksStopped = 5;
                 wristMotorStopPos = 0;
                 wristPos = 1;

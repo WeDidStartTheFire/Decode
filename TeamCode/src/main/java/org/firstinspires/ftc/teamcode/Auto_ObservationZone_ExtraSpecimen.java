4e5ad77e348a90be59c759873319a1613f07fbfe
Owen-FTC <27orinne@gmail.com>
2025-02-16 15:46:23 -0500

    Auto changes 1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_ObservationZone_ExtraSpecimen.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_ObservationZone_ExtraSpecimen.java
@@ -24,7 +24,7 @@ public class Auto_ObservationZone_ExtraSpecimen extends Base {
         running = true;
         Thread telemetryThread = new Thread(this::telemetryLoop);
         telemetryThread.start();
-        Thread driveThread = new Thread(() -> drive(29, BACKWARD));
+        Thread driveThread = new Thread(() -> drive(30, BACKWARD));
         Thread liftThread = new Thread(liftTask);
         Thread holdLift = new Thread(holdLiftTask);
         try {
@@ -60,63 +60,63 @@ public class Auto_ObservationZone_ExtraSpecimen extends Base {
             s(.5);
             moveVerticalLift(100);
 
-            Trajectory trajectory2 = drive.trajectoryBuilder(currentPose, true)
-                    .lineToLinearHeading(new Pose2d(-ROBOT_WIDTH / 2 + 2, 72 - ROBOT_LENGTH / 2 - 30.375 + 14, toRadians(90)))
-                    .splineToConstantHeading(new Vector2d(-ROBOT_WIDTH / 2 + 2, 72 - ROBOT_LENGTH / 2 - 30.375), toRadians(90))
-                    .build();
-            currentPose = trajectory2.end();
-            driveThread = new Thread(() -> drive.followTrajectory(trajectory2));
-            liftThread = new Thread(liftTask);
-            holdLift = new Thread(holdLiftTask);
-            driveThread.start();
-            s(.5);
-            liftThread.start();
-
-            liftThread.join();
-            holdLift.start();
-            driveThread.join();
-
-            hold = false;
-            holdLift.join();
-            moveVerticalLift(V_LIFT_GOALS[3] - 400);
-            openSpecimenServo();
-            s(.5);
-
-            Trajectory trajectory4 = drive.trajectoryBuilder(currentPose)
-                    .lineToLinearHeading(new Pose2d(-72 + ROBOT_WIDTH / 2, 72 - ROBOT_LENGTH / 2, toRadians(0)))
-                    .build();
-            currentPose = trajectory4.end();
-            liftThread = new Thread(this::retractVerticalLift);
-            liftThread.start();
-            drive.followTrajectory(trajectory4);
-            liftThread.join();
-            closeSpecimenServo();
-            s(.5);
-            Trajectory trajectory5 = drive.trajectoryBuilder(currentPose, true)
-                    .lineToLinearHeading(new Pose2d(-ROBOT_WIDTH / 2 + 4, 72 - ROBOT_LENGTH / 2 - 30.375, toRadians(90)))
-                    .build();
-            currentPose = trajectory5.end();
-            driveThread = new Thread(() -> drive.followTrajectory(trajectory5));
-            liftThread = new Thread(liftTask);
-            holdLift = new Thread(holdLiftTask);
-            moveVerticalLift(100);
-            driveThread.start();
-            s(.5);
-            liftThread.start();
-            liftThread.join();
-            holdLift.start();
-            driveThread.join();
-            hold = false;
-            holdLift.join();
-            moveVerticalLift(V_LIFT_GOALS[3] - 400);
-            openSpecimenServo();
-            s(.5);
-
-            Trajectory trajectory6 = drive.trajectoryBuilder(currentPose)
-                    .lineToLinearHeading(new Pose2d(-36 - 11, 72 - ROBOT_LENGTH, toRadians(-90)))
-                    .build();
-            currentPose = trajectory6.end();
-            drive.followTrajectory(trajectory6);
+//            Trajectory trajectory2 = drive.trajectoryBuilder(currentPose, true)
+//                    .lineToLinearHeading(new Pose2d(-ROBOT_WIDTH / 2 + 2, 72 - ROBOT_LENGTH / 2 - 30.375 + 14, toRadians(90)))
+//                    .splineToConstantHeading(new Vector2d(-ROBOT_WIDTH / 2 + 2, 72 - ROBOT_LENGTH / 2 - 30.375), toRadians(90))
+//                    .build();
+//            currentPose = trajectory2.end();
+//            driveThread = new Thread(() -> drive.followTrajectory(trajectory2));
+//            liftThread = new Thread(liftTask);
+//            holdLift = new Thread(holdLiftTask);
+//            driveThread.start();
+//            s(.5);
+//            liftThread.start();
+//
+//            liftThread.join();
+//            holdLift.start();
+//            driveThread.join();
+//
+//            hold = false;
+//            holdLift.join();
+//            moveVerticalLift(V_LIFT_GOALS[3] - 400);
+//            openSpecimenServo();
+//            s(.5);
+//
+//            Trajectory trajectory4 = drive.trajectoryBuilder(currentPose)
+//                    .lineToLinearHeading(new Pose2d(-72 + ROBOT_WIDTH / 2, 72 - ROBOT_LENGTH / 2, toRadians(0)))
+//                    .build();
+//            currentPose = trajectory4.end();
+//            liftThread = new Thread(this::retractVerticalLift);
+//            liftThread.start();
+//            drive.followTrajectory(trajectory4);
+//            liftThread.join();
+//            closeSpecimenServo();
+//            s(.5);
+//            Trajectory trajectory5 = drive.trajectoryBuilder(currentPose, true)
+//                    .lineToLinearHeading(new Pose2d(-ROBOT_WIDTH / 2 + 4, 72 - ROBOT_LENGTH / 2 - 30.375, toRadians(90)))
+//                    .build();
+//            currentPose = trajectory5.end();
+//            driveThread = new Thread(() -> drive.followTrajectory(trajectory5));
+//            liftThread = new Thread(liftTask);
+//            holdLift = new Thread(holdLiftTask);
+//            moveVerticalLift(100);
+//            driveThread.start();
+//            s(.5);
+//            liftThread.start();
+//            liftThread.join();
+//            holdLift.start();
+//            driveThread.join();
+//            hold = false;
+//            holdLift.join();
+//            moveVerticalLift(V_LIFT_GOALS[3] - 400);
+//            openSpecimenServo();
+//            s(.5);
+//
+//            Trajectory trajectory6 = drive.trajectoryBuilder(currentPose)
+//                    .lineToLinearHeading(new Pose2d(-36 - 11, 72 - ROBOT_LENGTH, toRadians(-90)))
+//                    .build();
+//            currentPose = trajectory6.end();
+//            drive.followTrajectory(trajectory6);
         } finally {
             running = false;
             hold = false;

0d5f379361d2bc20ffd04c78756481c7cfb26235
Owen-FTC <27orinne@gmail.com>
2025-07-03 09:45:35 -0500

    Improvements to RedundantLocalizer

--- TeamCode/src/main/java/pedroPathing/localizers/RedundantLocalizer.java
+++ TeamCode/src/main/java/pedroPathing/localizers/RedundantLocalizer.java
@@ -2,6 +2,7 @@ package pedroPathing.localizers;
 
 import com.pedropathing.localization.Localizer;
 import com.pedropathing.localization.Pose;
+import com.pedropathing.pathgen.MathFunctions;
 import com.pedropathing.pathgen.Vector;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
@@ -12,6 +13,11 @@ public class RedundantLocalizer extends Localizer {
 
     private final Localizer primaryLocalizer;
     private final Localizer secondaryLocalizer;
+    private Pose startPose;
+    private Pose pose;
+    private Pose vel;
+    private double totalHeading;
+    private double previousHeading;
 
     public RedundantLocalizer(HardwareMap map) {
         this(map, new Pose());
@@ -21,20 +27,17 @@ public class RedundantLocalizer extends Localizer {
         primaryLocalizer = MyPoseUpdater.createLocalizer(map, MyFollowerConstants.primaryLocalizer);
         secondaryLocalizer = MyPoseUpdater.createLocalizer(map, MyFollowerConstants.secondaryLocalizer);
         setStartPose(setStartPose);
+        pose = new Pose();
     }
 
     public Pose getPose() {
-        Pose primaryPose = primaryLocalizer.getPose();
-        if (primaryPose == null || primaryLocalizer.isNAN())
-            return secondaryLocalizer.getPose();
-        return primaryPose;
+        Vector vec = pose.getVector();
+        vec.rotateVector(this.startPose.getHeading());
+        return MathFunctions.addPoses(this.startPose, new Pose(vec.getXComponent(), vec.getYComponent(), pose.getHeading()));
     }
 
     public Pose getVelocity() {
-        Pose primaryVelocity = primaryLocalizer.getVelocity();
-        if (primaryVelocity == null || primaryLocalizer.isNAN())
-            return secondaryLocalizer.getVelocity();
-        return primaryVelocity;
+        return vel;
     }
 
     public Vector getVelocityVector() {
@@ -44,44 +47,46 @@ public class RedundantLocalizer extends Localizer {
     public void setStartPose(Pose setStart) {
         primaryLocalizer.setStartPose(setStart);
         secondaryLocalizer.setStartPose(setStart);
+        startPose = setStart;
     }
 
     public void setPose(Pose setPose) {
         primaryLocalizer.setPose(setPose);
         secondaryLocalizer.setPose(setPose);
+        pose = MathFunctions.subtractPoses(setPose, startPose);
     }
 
     public void update() {
         primaryLocalizer.update();
         secondaryLocalizer.update();
+        Pose primaryPose = primaryLocalizer.getPose();
+        if (primaryPose == null || primaryLocalizer.isNAN()) {
+            setPose(secondaryLocalizer.getPose());
+            vel = secondaryLocalizer.getVelocity();
+            totalHeading += MathFunctions.getSmallestAngleDifference(this.pose.getHeading(), this.previousHeading);
+            previousHeading = this.pose.getHeading();
+            return;
+        }
+        setPose(primaryPose);
+        vel = primaryLocalizer.getVelocity();
+        totalHeading += MathFunctions.getSmallestAngleDifference(this.pose.getHeading(), this.previousHeading);
+        previousHeading = this.pose.getHeading();
     }
 
     public double getTotalHeading() {
-        double primaryHeading = primaryLocalizer.getTotalHeading();
-        if (Double.isNaN(primaryHeading))
-            return secondaryLocalizer.getTotalHeading();
-        return primaryHeading;
+        return totalHeading;
     }
 
     public double getForwardMultiplier() {
-        double primaryMultiplier = primaryLocalizer.getForwardMultiplier();
-        if (Double.isNaN(primaryMultiplier))
-            return secondaryLocalizer.getForwardMultiplier();
-        return primaryMultiplier;
+        return secondaryLocalizer.getForwardMultiplier();
     }
 
     public double getLateralMultiplier() {
-        double primaryMultiplier = primaryLocalizer.getLateralMultiplier();
-        if (Double.isNaN(primaryMultiplier))
-            return secondaryLocalizer.getLateralMultiplier();
-        return primaryMultiplier;
+        return secondaryLocalizer.getLateralMultiplier();
     }
 
     public double getTurningMultiplier() {
-        double primaryMultiplier = primaryLocalizer.getTurningMultiplier();
-        if (Double.isNaN(primaryMultiplier))
-            return secondaryLocalizer.getTurningMultiplier();
-        return primaryMultiplier;
+        return secondaryLocalizer.getTurningMultiplier();
     }
 
     public void resetIMU() throws InterruptedException {

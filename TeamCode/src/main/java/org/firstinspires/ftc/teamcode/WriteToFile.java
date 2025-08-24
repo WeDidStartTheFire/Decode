0c73c10011642cc567d945901a76a152c8f838d5
Owen-FTC <27orinne@gmail.com>
2025-08-24 18:30:37 -0400

    Write to file pose changed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WriteToFile.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WriteToFile.java
@@ -1,12 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
+import static java.lang.Math.toRadians;
+
 import com.pedropathing.localization.Pose;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-@Autonomous(name="Write To File", group="Test")
+@Autonomous(name="Write To File", group="Test", preselectTeleOp="Test Odometry")
 public class WriteToFile extends Base {
     @Override
     public void runOpMode(){
-        saveOdometryPosition(new Pose(0,0,0));
+        saveOdometryPosition(new Pose(135, 81.5, toRadians(0)));
     }
 }

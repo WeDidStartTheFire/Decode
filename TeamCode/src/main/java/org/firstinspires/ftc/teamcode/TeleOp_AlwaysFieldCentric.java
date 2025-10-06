584a8114daded304465d8cf01de517dfcaf0275a
Owen-FTC <27orinne@gmail.com>
2025-10-05 21:04:56 -0400

    Added TeleOp_AlwaysFieldCentric

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_AlwaysFieldCentric.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode;
+
+import static org.firstinspires.ftc.teamcode.RobotState.validStartPose;
+import static org.firstinspires.ftc.teamcode.Utils.loadOdometryPosition;
+
+import com.pedropathing.geometry.Pose;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Always Field Centric", group = "Main")
+public class TeleOp_AlwaysFieldCentric extends OpMode {
+    public TeleOpFunctions teleop;
+    public Robot robot;
+    public TelemetryUtils tm;
+
+    @Override
+    public void init() {
+        Pose pose = loadOdometryPosition();
+        validStartPose = pose != null;
+        RobotState.pose = validStartPose ? pose : new Pose();
+        robot = new Robot(hardwareMap, telemetry, validStartPose);
+        robot.follower.setPose(RobotState.pose);
+        robot.follower.startTeleopDrive();
+        teleop = new TeleOpFunctions(robot, gamepad1, gamepad2);
+        tm = robot.drivetrain.tm;
+        if (!validStartPose)
+            tm.print("Field Centric Driving️", "☑️Will be used without valid position");
+        else tm.print("Field Centric Driving", "✅");
+    }
+
+    @Override
+    public void loop() {
+        teleop.update();
+        teleop.drivetrainLogic(true);
+        teleop.feederLogic();
+        teleop.launcherLogic();
+        teleop.intakeLogic();
+    }
+}

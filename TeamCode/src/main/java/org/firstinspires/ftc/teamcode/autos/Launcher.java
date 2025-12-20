901c04797be8aea9ac16312c314d90513f501f3d
Owen-FTC <27orinne@gmail.com>
2025-12-20 18:32:28 -0500

    Update Launcher.java with more robust code created last practice

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/Launcher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/Launcher.java
@@ -1,11 +1,11 @@
 package org.firstinspires.ftc.teamcode.autos;
 
+import static org.firstinspires.ftc.teamcode.RobotConstants.INDEXER_SPEED;
 import static org.firstinspires.ftc.teamcode.RobotConstants.MAX_LAUNCHER_SPIN_WAIT;
 
 import com.pedropathing.util.Timer;
 
 import org.firstinspires.ftc.teamcode.Robot;
-import org.firstinspires.ftc.teamcode.RobotConstants;
 
 public class Launcher {
 
@@ -47,7 +47,8 @@ public class Launcher {
                 break;
             case ROTATE_INDEX:
                 robot.retractFeeder();
-                if (robot.isFeederUp()) break;
+                if ((robot.isFeederUp() || stateTimer.getElapsedTimeSeconds() < .2) &&
+                        stateTimer.getElapsedTimeSeconds() < .6) break;
                 double goalPos = ((3 - artifactsToLaunch) % 3) / 2.0;
                 robot.setIndexerServoPos(goalPos);
                 setState(State.PUSH_ARTIFACT);
@@ -55,13 +56,15 @@ public class Launcher {
             case PUSH_ARTIFACT:
                 robot.spinLaunchMotors();
                 if (!robot.isIndexerStill() || (!robot.launchMotorsToSpeed() &&
-                        stateTimer.getElapsedTimeSeconds() < MAX_LAUNCHER_SPIN_WAIT))
-                    break;
+                        stateTimer.getElapsedTimeSeconds() < MAX_LAUNCHER_SPIN_WAIT) ||
+                        stateTimer.getElapsedTimeSeconds() < .2 || (robot.getInches() == 6 &&
+                        stateTimer.getElapsedTimeSeconds() < 1 / INDEXER_SPEED)) break;
                 robot.pushArtifactToLaunch();
                 setState(State.RETRACT_FEEDER);
                 break;
             case RETRACT_FEEDER:
-                if (robot.getArtifact() != RobotConstants.Artifact.UNKNOWN) break;
+                if ((robot.getInches() != 6 || stateTimer.getElapsedTimeSeconds() < .2) &&
+                        stateTimer.getElapsedTimeSeconds() < 2) break;
                 artifactsToLaunch--;
                 robot.retractFeeder();
                 if (artifactsToLaunch > 0) setState(State.ROTATE_INDEX);

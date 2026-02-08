ed1882d3a8ae6ee144cd63827f8596bb7e5874f1
ftc-payton <paytonbrian1126@gmail.com>
2026-02-08 13:31:02 -0500

    worst program oat

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Diagnostics.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Diagnostics.java
@@ -11,6 +11,7 @@ import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 
 import org.firstinspires.ftc.teamcode.robot.HardwareInitializer;
+import org.firstinspires.ftc.teamcode.robot.mechanisms.Indexer;
 
 @TeleOp(name = "Diagnostics", group= "A")
 public class Diagnostics extends LinearOpMode {
@@ -21,6 +22,7 @@ public class Diagnostics extends LinearOpMode {
     public ColorSensor colorSensor;
     public TouchSensor turretTouchSensor;
     public SparkFunOTOS otos;
+    public Indexer indexer;
     
     public TelemetryUtils tm;
     
@@ -28,51 +30,51 @@ public class Diagnostics extends LinearOpMode {
         tm = new TelemetryUtils(telemetry);
         lf = HardwareInitializer.init(hardwareMap, DcMotorEx.class, "leftFront");
         tm.print("Left Front Motor",
-                lf == null ? "⚠️ Not Connected (CH Motor Port 1)" : "✅ Connected");
+                lf == null ? "⚠️ Not Connected (CH Motor 1)" : "✅ Connected");
         lb = HardwareInitializer.init(hardwareMap, DcMotorEx.class, "leftBack");
         tm.print("Left Back Motor",
-                lb == null ? "⚠️ Not Connected (CH Motor Port 3)" : "✅ Connected");
+                lb == null ? "⚠️ Not Connected (CH Motor 3)" : "✅ Connected");
         rf = HardwareInitializer.init(hardwareMap, DcMotorEx.class, "rightFront");
         tm.print("Right Front Motor",
-                rf == null ? "⚠️ Not Connected (CH Motor Port 0)" : "✅ Connected");
+                rf == null ? "⚠️ Not Connected (CH Motor 0)" : "✅ Connected");
         rb = HardwareInitializer.init(hardwareMap, DcMotorEx.class, "rightBack");
         tm.print("Right Back Motor",
-                rb == null ? "⚠️ Not Connected (CH Motor Port 2)" : "✅ Connected");
+                rb == null ? "⚠️ Not Connected (CH Motor 2)" : "✅ Connected");
 
         feederServoA = HardwareInitializer.init(hardwareMap, Servo.class, "feederServoA");
         tm.print("Feeder Servo A",
-                feederServoA == null ? "⚠️ Not Connected (EH Servo Port 0)" : "✅ Connected");
+                feederServoA == null ? "⚠️ Not Connected (EH Servo 0)" : "✅ Connected");
         feederServoB = HardwareInitializer.init(hardwareMap, Servo.class, "feederServoB");
         tm.print("Feeder Servo B",
-                feederServoB == null ? "⚠️ Not Connected (EH Servo Port 1)" : "✅ Connected");
+                feederServoB == null ? "⚠️ Not Connected (EH Servo 1)" : "✅ Connected");
 
         indexerServo = HardwareInitializer.init(hardwareMap, Servo.class, "indexerServo");
         tm.print("Indexer Servo",
-                indexerServo == null ? "⚠️ Not Connected (EH Servo Port 2)" : "✅ Connected");
+                indexerServo == null ? "⚠️ Not Connected (EH Servo 2)" : "✅ Connected");
 
         intakeMotor = HardwareInitializer.init(hardwareMap, DcMotorEx.class, "intakeMotor");
         tm.print("Intake Motor",
-                intakeMotor == null ? "⚠️ Not Connected (EH Motor Port 0)" : "✅ Connected");
+                intakeMotor == null ? "⚠️ Not Connected (EH Motor 0)" : "✅ Connected");
         intakeServoA = HardwareInitializer.init(hardwareMap, CRServo.class, "intakeServoA");
         tm.print("Intake Servo A",
-                intakeServoA == null ? "⚠️ Not Connected (EH Servo Port 3)" : "✅ Connected");
+                intakeServoA == null ? "⚠️ Not Connected (EH Servo 3)" : "✅ Connected");
         intakeServoB = HardwareInitializer.init(hardwareMap, CRServo.class, "intakeServoB");
         tm.print("Intake Servo B",
-                intakeServoB == null ? "⚠️ Not Connected (EH Servo Port 4)" : "✅ Connected");
+                intakeServoB == null ? "⚠️ Not Connected (EH Servo 4)" : "✅ Connected");
         intakeServoC = HardwareInitializer.init(hardwareMap, CRServo.class, "intakeServoC");
         tm.print("Intake Servo C",
-                intakeServoC == null ? "⚠️ Not Connected (EH Servo Port 5)" : "✅ Connected");
+                intakeServoC == null ? "⚠️ Not Connected (EH Servo 5)" : "✅ Connected");
 
         launcherMotorA = HardwareInitializer.init(hardwareMap, DcMotorEx.class, "launcherMotorA");
         tm.print("Launcher Motor A",
-                launcherMotorA == null ? "⚠️ Not Connected (EH Motor Port 1)" : "✅ Connected");
+                launcherMotorA == null ? "⚠️ Not Connected (EH Motor 1)" : "✅ Connected");
         launcherMotorB = HardwareInitializer.init(hardwareMap, DcMotorEx.class, "launcherMotorB");
         tm.print("Launcher Motor B",
-                launcherMotorB == null ? "⚠️ Not Connected (EH Motor Port 2)" : "✅ Connected");
+                launcherMotorB == null ? "⚠️ Not Connected (EH Motor 2)" : "✅ Connected");
 
         led = HardwareInitializer.init(hardwareMap, Servo.class, "led");
         tm.print("LED",
-                led == null ? "⚠️ Not Connected (CH Servo Port 0)" : "✅ Connected");
+                led == null ? "⚠️ Not Connected (CH Servo 0)" : "✅ Connected");
 
         limelight = HardwareInitializer.init(hardwareMap, Limelight3A.class, "limelight");
         tm.print("Limelight",
@@ -80,20 +82,66 @@ public class Diagnostics extends LinearOpMode {
 
         colorSensor = HardwareInitializer.init(hardwareMap, ColorSensor.class, "colorSensor");
         tm.print("Color Sensor",
-                colorSensor == null ? "⚠️ Not Connected (CH I2C Port 2)" : "✅ Connected");
+                colorSensor == null ? "⚠️ Not Connected (CH I2C 2)" : "✅ Connected");
 
         turretMotor = HardwareInitializer.init(hardwareMap, DcMotorEx.class, "turretMotor");
         tm.print("Turret Motor",
-                turretMotor == null ? "⚠️ Not Connected (EH Motor Port 3)" : "✅ Connected");
+                turretMotor == null ? "⚠️ Not Connected (EH Motor 3)" : "✅ Connected");
         turretTouchSensor = HardwareInitializer.init(hardwareMap, TouchSensor.class, "turretTouchSensor");
         tm.print("Turret Touch Sensor",
-                turretTouchSensor == null ? "⚠️ Not Connected (CH Digital Port 2:3)" : "✅ Connected");
+                turretTouchSensor == null ? "⚠️ Not Connected (CH Digital 2:3)" : "✅ Connected");
 
         otos = HardwareInitializer.init(hardwareMap, SparkFunOTOS.class, "sensorOtos");
         tm.print("SparkFun Sensor",
-                otos == null ? "⚠️ Not Connected (CH I2C Port 1)" : "✅ Connected");
+                otos == null ? "⚠️ Not Connected (CH I2C 1)" : "✅ Connected");
+
+        indexer = new Indexer(hardwareMap, tm, null, null);
 
         tm.update();
         waitForStart();
+
+        if (feederServoA != null && feederServoB != null) {
+            feederServoB.setPosition(.85);
+            feederServoA.setPosition(.85);
+            actionTm("Raising feeder servo", "Lowering feeder servo");
+            sleep(1000);
+            feederServoA.setPosition(0);
+            feederServoB.setPosition(0);
+            actionTm("Lowering feeder servo", "Indexer to second position");
+            sleep(1000);
+        }
+
+        if (indexerServo != null){
+            indexer.setPos(0.5);
+            actionTm("Indexer to second position", "Indexer to third position");
+            sleep(1000);
+            indexer.setPos(1);
+            actionTm("Indexer to third position", "Indexer to first position");
+            sleep(1000);
+            indexer.setPos(0);
+            actionTm("Indexer to first position", "Intake moving");
+            sleep(1000);
+        }
+
+        if (intakeServoA != null) intakeServoA.setPower(1);
+        if (intakeServoC != null) intakeServoC.setPower(1);
+        if (intakeMotor != null) intakeMotor.setPower(1);
+        if (intakeServoB != null) intakeServoB.setPower(1);
+        actionTm("Intake moving", "Spinning launcher motors");
+        sleep(1000);
+
+        if (launcherMotorA != null) launcherMotorA.setVelocity(1000);
+        if (launcherMotorB != null) launcherMotorB.setVelocity(1000);
+        actionTm("Spinning launcher motors", "End");
+        sleep(5000);
+
+        if (launcherMotorA != null) launcherMotorA.setVelocity(0);
+        if (launcherMotorB != null) launcherMotorB.setVelocity(0);
+    }
+
+    private void actionTm(String current, String next){
+        tm.print("Currently", current);
+        tm.print("Next", next);
+        tm.update();
     }
 }

d8556603ec17321226fae11e38b832c3123bcad8
John Carter Thompson <jc.play12341234@gmail.com>
2026-09-13 17:01:54 -0400

    Added more to my code/changed repeated lines to functions.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/tests/TeleOp_JCTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/tests/TeleOp_JCTest.java
@@ -3,10 +3,12 @@ package org.firstinspires.ftc.teamcode.teleops.tests;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class TeleOp_JCTest extends OpMode {
     DcMotorEx rightBack, rightFront, leftBack, leftFront;
+    Servo servoOne;
     double startTime, currentTime;
 
     @Override
@@ -15,41 +17,41 @@ public class TeleOp_JCTest extends OpMode {
         rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
         leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
         leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
+        servoOne = hardwareMap.get(Servo.class, "servoOne");
         telemetry.addLine("Initialization completed");
+        telemetry.update();
+    }
+
+    public void pause(double time) {
+        startTime = getRuntime();
+        while (currentTime < startTime + time) {
+            currentTime = getRuntime();
+        }
+    }
+
+    public void printPower() {
+        telemetry.addData("rightBack", rightBack.getPower());
+        telemetry.addData("leftBack", leftBack.getPower());
+        telemetry.addData("rightFront", rightFront.getPower());
+        telemetry.addData("leftFront", leftFront.getPower());
         telemetry.update();
     }
 
     @Override
     public void start() {
-        startTime = getRuntime();
-        while (currentTime < startTime + .1) {
-            currentTime = getRuntime();
-        }
+        pause(.1);
         rightBack.setPower(.85);
-        telemetry.addData("rightBack", rightBack.getPower());
-        telemetry.addLine("test");
-        telemetry.update();
-        startTime = getRuntime();
-        while (currentTime < startTime + 3.65) {
-            currentTime = getRuntime();
-            telemetry.addData("rightBack", rightBack.getPower());
-            telemetry.addLine("test");
-            telemetry.update();
-        }
+        printPower();
+        pause(3.65);
         rightBack.setPower(-.2);
         leftBack.setPower(.5);
-        telemetry.addData("leftBack", leftBack.getPower());
-        telemetry.addData("rightBack", rightBack.getPower());
-        telemetry.update();
-        startTime = getRuntime();
-        while (currentTime < startTime + 2.05) {
-            currentTime = getRuntime();
-        }
+        printPower();
+        pause(2.05);
         rightBack.setPower(0);
         leftBack.setPower(0);
-        telemetry.addData("leftBack", leftBack.getPower());
-        telemetry.addData("rightBack", rightBack.getPower());
-        telemetry.update();
+        printPower();
+        servoOne.setPosition(0.99);
+        pause((2+(17 / 19.0) / (2048/(1/17.0)*(1 / 125000.0))));
     }
 
     @Override
